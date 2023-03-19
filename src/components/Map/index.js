9591bc79f21210f7b9179669cd6baa6e452d3450
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-03-19 19:46:19 +0200

    Added custom marker icon.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,18 +1,26 @@
-// You will need to install - npm install leaflet react-leaflet
-
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon from './marker-icon.png';
+// import markerShadow from './marker-shadow.png';
 
 function Map() {
   const position = [51.505, -0.09]; // London coordinates
 
+  const customMarkerIcon = L.icon({
+    iconUrl: markerIcon,
+    iconSize: [50, 50],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -38],
+  });
+
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100vh' }}>
+    <MapContainer center={position} zoom={20} scrollWheelZoom={false} style={{ height: '100vh' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
+      <Marker position={position} icon={customMarkerIcon}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          I am a popup.... <br /> that is easily customisable.
         </Popup>
       </Marker>
     </MapContainer>
@@ -20,4 +28,3 @@ function Map() {
 }
 
 export default Map;
-
