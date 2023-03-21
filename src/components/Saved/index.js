2f63d9979e82d3a3c73ef9bac27c88992a425790
talha731 <118377080+talha731@users.noreply.github.com>
2023-03-21 00:23:41 +0000

    added the js for saved

--- src/components/Saved/index.js
+++ src/components/Saved/index.js
@@ -1,8 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function SavedTrips(props) {
+  const { currentUser } = props;
+  const [trips, setTrips] = useState([]);
 
-export const Saved = () => {
+  useEffect(() => {
+    const savedUser = JSON.parse(localStorage.getItem('user'));
+    if (savedUser) {
+      axios.get('/api/saved-trips')
+        .then(response => {
+          setTrips(response.data.filter(trip => trip.user === savedUser));
+        })
+        .catch(error => console.log(error));
+    }
+  }, []);
 
-    return(
-        <h1>Saved</h1>
-    )
+  const handleDeleteTrip = (tripId) => {
+    axios.delete(`/api/saved-trips/${tripId}`)
+      .then(response => {
+        setTrips(trips.filter(trip => trip.id !== tripId));
+      })
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Your Saved Trips</h1>
+      <ul>
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <h2>{trip.title}</h2>
+            <p>{trip.description}</p>
+            <button onClick={() => handleDeleteTrip(trip.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default SavedTrips;
