4708598f685c827e83e4bac40ee3d19f475d5d67
jamellknows <jamellsamuels@googlemail.com>
2023-03-23 07:44:51 +0000

    added github links to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,12 @@ import "./styles.css"
 import { useState } from "react";
 import Modal from 'react-modal'
 import { PrivacyPolicy } from "./Privacy";
+import { GithubLinks } from "./GithubLinks";
 
 const Footer = () => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
-
+    const [modalIsOpen2, setModalIsOpen2] = useState(false);
 
     const setModalIsOpenToTrue = () => {
         setModalIsOpen(true);
@@ -16,21 +17,34 @@ const Footer = () => {
         setModalIsOpen(false);
     }
 
+    const setModalIsOpenToTrue2 = () => {
+        setModalIsOpen2(true);
+    }
+
+    const setModalIsOpenToFalse2 = () =>{
+        setModalIsOpen2(false);
+    }
+
 
 
     return(
         <>
         <footer className="container-fluid footer fixed-bottom">
-            <div className="d-flex flex-row">
-                <div className="col-6">
+            <div className="d-inline-block">
+                {/* <div className="col-6"> */}
                     <ul>
-                        <li><button type="button" className="btn btn-info footer-button" onClick={setModalIsOpenToTrue}>Privacy Policy</button></li>
+                        <li className="list-item"><button type="button" className="btn btn-info footer-button" onClick={setModalIsOpenToTrue}>Github Links</button></li>
                         <Modal isOpen={modalIsOpen} ariaHideApp={false} preventScroll={false}>
                             <button className="btn btn-info" onClick={setModalIsOpenToFalse}>X</button>
+                            <GithubLinks/>
+                        </Modal>
+                    <li><button type="button" className="btn btn-info footer-button" onClick={setModalIsOpenToTrue2}>Privacy Policy</button></li>
+                        <Modal isOpen={modalIsOpen2} ariaHideApp={false} preventScroll={false}>
+                            <button className="btn btn-info" onClick={setModalIsOpenToFalse2}>X</button>
                             <PrivacyPolicy/>
                         </Modal>
                     </ul>
-                </div>
+                {/* </div> */}
                
             </div>
         </footer>
